b91d657f964f78305171622be46e2d1cb7884b3e
Julia ATES <jates-@e1r9p7.42.fr>
2021-08-08 04:42:34 +0200

    Still working on histogram

--- histogram.py
+++ histogram.py
@@ -5,6 +5,21 @@ import sys
 import numpy
 from describe.py import describe 
 
+def homogeneity_table(df, fields, houses, type_calc):
+    homo = {}
+    homo['Total Courses'] = [0] * (len(houses)+1)
+    for field in fields[2:]:
+        sum = 0
+        homo[field] = []
+        for house in houses:
+            sum += df[house][field][type_calc]
+            homo[field].append(df[house][field][type_calc])    
+        homo[field].append(sum)
+        homo['Total Courses'] = [homo['Total Courses'][i] + homo[field][i] for i in range(len(homo[field]))]
+
+    heterogeniety_df = pd.DataFrame(homo)
+    heterogeniety_df.index = houses + ['Total Students']
+    return (heterogeniety_df)
 
 
 def myvar(col):
@@ -33,13 +48,24 @@ if __name__ == "__main__":
             print("data is corrupted")
         
         house_data = {}
+        homogeneity_data = {}
         for house in houses:
-            data = df.loc[df['Hogwarts House'] == house]
-            numeric_features = []
-            for c in data.columns:
-                col_type = data[c].dtype
-                if col_type == float or col_type == int:
-                    numeric_features.append(c)
-            describe_df = describe(house_data, numeric_features)
-            house_data[house] = [describe_df]
+             homogeneity_data[house] = df.loc[df['Hogwarts House'] == house]
+             numeric_features = []
+             for c in df.columns:
+                 col_type = df[c].dtype
+                 if col_type == float or col_type == int:
+                     numeric_features.append(c)
+             describe_df = describe(homogeneity_data[house], numeric_features)
+             house_data[house] = describe_df
         
+        #Total all house:
+        numeric_features = []
+        for c in df.columns:
+            col_type = df[c].dtype
+            if col_type == float or col_type == int:
+                numeric_features.append(c)
+        describe_df = describe(df, numeric_features)
+        house_data['Total Student'] = describe_df
+        
+        homongeneity_df = homogeneity_table(df, numeric_features, houses, 'count')
